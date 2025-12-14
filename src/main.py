ddea1ee89841f21453b9cc99b9cea3146d4c57bb
rocas <rocasdev@proton.me>
2025-12-15 00:40:53 +0200

    chore(main): bump version to 2.0.0

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ Defines routes and dependency injection for database sessions.
 
 from fastapi import FastAPI, Depends, HTTPException, status
 from src.database import Base, engine, SessionLocal
+
 from src.recipes.routes import router as recipes_router
 
 from fastapi.middleware.cors import CORSMiddleware
@@ -14,7 +15,7 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Tarmo",
-    version="0.0.1",
+    version="2.0.0",
     description="Optimize and control batches with recipes."
 )
 
